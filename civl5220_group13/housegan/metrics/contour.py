753e455c332e8ea502cc59308d62062a41373713
enhuiz <niuzhe.nz@outlook.com>
2021-12-11 17:50:29 +0800

    add bbox iou

--- civl5220_group13/housegan/metrics/contour.py
+++ civl5220_group13/housegan/metrics/contour.py
@@ -1,13 +1,28 @@
+"""
+Contour Metrics based on IOU, thanks to Changyang.
+"""
+
 import cv2
 import numpy as np
+from dataclasses import dataclass
 
-from .base import MetricBase
 
+@dataclass
+class ContourMetrics:
+    im_size: int = 256
 
-class ContourMetrics(MetricBase):
-    """
-    intersection over union
-    """
+    def draw_boxes(self, boxes):
+        im_size = self.im_size
+        ret = np.zeros((im_size, im_size))
+        for box in boxes:
+            # all values should be within [0, 1]
+            # note that Python support 0 <= n <= 1
+            # no need to 0 <= n and n <= 1
+            if all([0 <= n <= 1 for n in box]):
+                scale = lambda n: int(im_size * n)
+                x0, y0, x1, y1 = map(scale, box)
+                ret[y0:y1, x0:x1] = 1
+        return ret
 
     def __call__(self, boxes, real):
         """
--- civl5220_group13/housegan/metrics/quality.py
+++ civl5220_group13/housegan/metrics/quality.py
@@ -1,15 +1,17 @@
+"""
+Quality metrics, thanks to Kai.
+"""
+
 import numpy as np
 from itertools import combinations
-
 from ..utils import ROOM_CLASS
-from .base import MetricBase
 
 
 def inversed(l):
     return [ROOM_CLASS.inverse[e] for e in l]
 
 
-class QualityMetrics(MetricBase):
+class QualityMetrics:
     separate_list = inversed(
         [
             "living_room",
@@ -34,6 +36,21 @@ class QualityMetrics(MetricBase):
             "closet",
         ]
     )
+
+    def bbox_iou(self, bbox1, bbox2):
+        cx1, cy1, cx2, cy2 = bbox1
+        gx1, gy1, gx2, gy2 = bbox2
+
+        carea = (cx2 - cx1) * (cy2 - cy1)
+        garea = (gx2 - gx1) * (gy2 - gy1)
+
+        x1, y1, x2, y2 = max(cx1, gx1), max(cy1, gy1), min(cx2, gx2), min(cy2, gy2)
+        w, h = max(0, x2 - x1), max(0, y2 - y1)
+
+        area = w * h
+        iou = area / (carea + garea - area)
+
+        return iou
 
     def get_node_type(self, node):
         if node in self.separate_list:
@@ -74,11 +91,7 @@ class QualityMetrics(MetricBase):
                 iou_is_good = True
 
             bi, bj = boxes[[i, j]]
-
-            # box2mask
-            mi, mj = map(self.draw_box, [bi, bj])
-
-            iou = (mi & mj).sum() / (mi | mj).sum()
+            iou = self.bbox_iou(bi, bj)
 
             if iou_is_good:
                 scores.append(iou)
@@ -90,3 +103,14 @@ class QualityMetrics(MetricBase):
 
 if __name__ == "__main__":
     metric = QualityMetrics()
+    score = metric(
+        np.array([1, 2, 3]),
+        np.array(
+            [
+                [0, 0, 1, 1],
+                [0.5, 0.2, 1, 1],
+                [0.5, 0.2, 1, 1],
+            ]
+        ),
+    )
+    print(score)
