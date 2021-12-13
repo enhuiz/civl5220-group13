bb7d51a6c9466dab7cb23a0821b8345e4261dbc3
enhuiz <niuzhe.nz@outlook.com>
2021-12-13 09:17:16 +0800

    update

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 data=3rdparty/housegan/FloorplanDataset/eval_data.npy
-args='-m civl5220_group13.__init__ housegan constraint_inference $data'
+args="-m civl5220_group13.__init__ housegan constraint_inference $data"
 
-python $args --contour ./evaluation/contours/irregular.txt --output irregular
-python $args --contour ./evaluation/contours/rectangle.txt --output rectangle
+# python $args --contour ./evaluation/contours/irregular.txt --output irregular
+# python $args --contour ./evaluation/contours/rectangle.txt --output rectangle
 
 python $args --contour ./evaluation/contours/irregular.txt --output irregular-gcs --criterions nlccs ciou
-python $args --contour ./evaluation/contours/rectangle.txt --output rectangle-gqs --criterions nlccs ciou
+python $args --contour ./evaluation/contours/rectangle.txt --output rectangle-gcs --criterions nlccs ciou
 
-python $args --contour ./evaluation/contours/irregular.txt --output irregular-gcs --criterions nlccs fiou
+python $args --contour ./evaluation/contours/irregular.txt --output irregular-gqs --criterions nlccs fiou
 python $args --contour ./evaluation/contours/rectangle.txt --output rectangle-gqs --criterions nlccs fiou
-
